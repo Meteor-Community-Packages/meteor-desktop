913f5024742b78526f08a384eab32f0d1c20d20d
Jan Dvorak <storytellercz@proton.me>
2024-08-19 16:44:39 +0200

    Initial ideation for #15 Add Typescript support

--- plugins/bundler/package.js
+++ plugins/bundler/package.js
@@ -1,7 +1,7 @@
 /* eslint-disable prefer-arrow-callback */
 Package.describe({
     name: 'communitypackages:meteor-desktop-bundler',
-    version: '3.2.0',
+    version: '3.3.0',
     summary: 'Bundles .desktop dir into desktop.asar.',
     git: 'https://github.com/Meteor-Community-Packages/meteor-desktop',
     documentation: 'README.md'
@@ -9,13 +9,13 @@ Package.describe({
 
 Package.registerBuildPlugin({
     name: 'meteor-desktop-bundler',
-    use: ['ecmascript@0.16.1'],
+    use: ['ecmascript@0.16.9', 'typescript@4.9.5'],
     sources: ['bundler.js'],
     npmDependencies: { chokidar: '3.5.3' }
 });
 
 Package.onUse(function onUse(api) {
-    api.versionsFrom('1.4.4.6');
+    api.versionsFrom(['2.8.1', '2.16']);
     api.use('isobuild:compiler-plugin@1.0.0');
     api.addFiles([
         'version._desktop_.js'
